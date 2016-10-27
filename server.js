caa46d0bb0fa04bbf53a6927fb63c9d49095da46
KalyanKalluri <kalyankalluri999@gmail.com>
2016-10-27 18:34:51 +0545

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-one'));
 });
 app.get('/article-two', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-two'));
 });
 app.get('/article-three', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-three'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
