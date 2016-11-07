6c317670df1e643e6bef18501baea0c5155c8576
KalyanKalluri <kalyankalluri999@gmail.com>
2016-11-07 21:40:09 +0545

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,24 +1,67 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool =require('pg').pool;
+
+var config =
+{
+    user:     'kalyan kalluri'
+    database: 'kalyan kalluri'
+    host: 'db.imad.hasura-app.io',
+    password: DB-password
+}''
 
 var app = express();
 app.use(morgan('combined'));
 
+var articleone{
+    titile: 'articleone | Kalluri sai kalyan',
+    heading: 'Article one',
+    date: 'Nov5,2016'
+    content:
+    <p>
+    This is content for my first article. This is content for my first web app
+    </p>
+}
+function createTemplate(data){
+ var html template=
+<html>
+<head>
+<title>
+${title}
+</title>
+<meta name="viewpoint" content="width=device-width,Initial-Scale=1"/>
+        <link href="/ui/style.css" rel="style sheet"/>
+        </head>
+        <body>
+        <divclass="container">
+        </div>
+         <a> href="/">Home</a>
+         </div>
+         <hr/>
+         <h3>
+         ${heading}
+         </h3>
+         <div>
+         ${date}
+         </div>
+         <div>
+         ${content}
+         </div>
+         </body>
+         </html>
+         ;
+         return html template
+}
+
+var articleone = {
+    <p>
+        This is content for my first article. This is content for my first web app
+        </p>
+        }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/articleone', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/articletwo', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/articlethree', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
