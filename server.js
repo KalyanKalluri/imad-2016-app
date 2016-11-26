0b53e82b5eafa84f353d3fb7d1158fe6e66fd990
KalyanKalluri <kalyankalluri999@gmail.com>
2016-11-26 07:30:51 +0545

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app=express();
 app.use(morgon(combined));
 
- app.get('/')function (req, res){
+ app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/ui/style.css', function (req, res) {
